4099c209a1396e666da19ad3b97a10d4c03e37b0
Иван Иван <remillius_dies@outlook.com>
2019-11-02 12:50:36 +0300

    initial

---
+++ demoInterface/Complex.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace demoInterface
+{
+    public class Complex : IComparable<Complex>
+    {
+        public Complex(double re, double im)
+        {
+            Re = re;
+            Im = im;
+        }
+        public double Re { get; set; }
+        public double Im { get; set; }
+        public int CompareTo(Complex other)
+        {
+            if (other.Re < Re) return 1;
+            if (other.Re > Re) return -1;
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Re}+{Im}i";
+        }
+    }
+}
--- demoInterface/Program.cs
+++ demoInterface/Program.cs
@@ -10,6 +10,32 @@ namespace demoInterface
     {
         static void Main(string[] args)
         {
+            int[] arrInt = new[] {1, 2, 4, 6, 57, 4, 2};
+            Array.Sort(arrInt);
+
+            for (int i = 0; i < arrInt.Length; i++)
+            {
+                Console.WriteLine(arrInt[i]);
+            }
+
+
+            Complex[] arrComp = new Complex[]
+            {
+                new Complex(122, 2),
+                new Complex(2, 2),
+                new Complex(3, 2),
+                new Complex(2, 2),
+                new Complex(12, 2),
+                new Complex(22, 2),
+            };
+            Array.Sort(arrComp);
+            for (int i = 0; i < arrComp.Length; i++)
+            {
+                Console.WriteLine(arrComp[i]);
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
